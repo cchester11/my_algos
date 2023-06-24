b712fda5e9df0eb6fdfea6b59e8bec97b2c53934
Charles <cchester1@angelo.edu>
2023-06-24 15:54:44 -0500

    not able to write from command line outside of directory and need to work on the formatting of the json when written to

--- storage.js
+++ storage.js
@@ -2,32 +2,38 @@ const fs = require('fs')
 const path = require('path')
 
 function storeAlgorithm(name, date, code) {
-      const filePath = path.join(__dirname, 'algoData.json')
+      const filePath = path.resolve(__dirname, 'algoData.json')
 
-      let data = []
-      if (fs.existsSync('/algoData.json')) {
-            const contents = fs.readFileSync(filePath, 'utf8')
-            storage = JSON.parse(contents)
+      if (path.isAbsolute(filePath)) {
+            let data = []
+            if (fs.existsSync('/algoData.json')) {
+                  const contents = fs.readFileSync(filePath, 'utf8')
+                  storage = JSON.parse(contents)
+            }
+
+            data.push({ name, date, code })
+
+            const dataToJSON = JSON.stringify(data, null, 2)
+
+            fs.writeFileSync('./algoData.json', dataToJSON, {
+                  encoding: 'utf8',
+                  flag: 'a'
+            })
+
+            console.log(filePath)
+            console.log('Algorithm(s) saved to storage file')
+      } else {
+            return 'Incohesive file path'
       }
-
-      data.push({ name, date, code })
-
-      const dataToJSON = JSON.stringify(data, null, 2)
-
-      fs.writeFileSync('./algoData.json', dataToJSON, { 
-            encoding: 'utf8',
-            flag: 'a' 
-      })
-
-      console.log('Algorithm(s) saved to storage file')
 }
 
 const algoName = process.argv[2]
 const algoDate = process.argv[3]
 const algoCode = process.argv[4]
 
-if(algoName && algoDate && algoCode) {
+if (algoName && algoDate && algoCode) {
       storeAlgorithm(algoName, algoDate, algoCode)
 } else {
+      console.log(__dirname)
       console.log('Please provide the name under which you like to the algorithm to be stored, the date, and the code itself--in that order.')
 }
