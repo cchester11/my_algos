44409ba17ea43a14cd779fde958974d449afb95b
Charles <cchester1@angelo.edu>
2023-10-25 20:48:50 -0500

    checkPath module

--- utilities/checkPath.js
+++ utilities/checkPath.js
@@ -4,16 +4,22 @@ const fs = require('fs');
 /**
  * Determines whether or not a path is valid.
  * @param {(string)} xPath 
- * @returns {boolean}
+ * @returns {string}
  */
 function checkPath (xPath) {
+      if(typeof xPath !== 'string') {
+            throw new Error("path is not in string from")
+      }
+
       if(path.isAbsolute(xPath)) {
             if(fs.existsSync(xPath)) {
-                  return true
+                  return "exist sync"
+            } else {
+                  return "no exist sync"
             }
       }
 
-      return false
+      return "Incohesive file path."
 }
 
 module.exports = checkPath;
--- utilities/store_algorithm.js
+++ utilities/store_algorithm.js
@@ -15,63 +15,63 @@ function storeAlgorithm(name, date, difficulty, code) {
       const filePath = path.join(__dirname, '../json/algoData.json')
       console.log('Path to algorithm storage json file: ' + filePath)
 
-      // check for errors in path
-      if (path.isAbsolute(filePath)) {
-            // read the storage file
-            let storage;
-            if (fs.existsSync(filePath)) {
-                  const contents = fs.readFileSync(filePath, 'utf8')
-                  storage = JSON.parse(contents)
-            } else {
-                  storage = {
-                        algorithm_storage: []
-                  }
-                  console.log('no sync exist')
+      let storage;
+
+      console.log(checkPath(filePath))
+
+      if ((checkPath(filePath)) === "Incohesive file path.") {
+            throw new Error("Incohesive file path.")
+      } else if ((checkPath(filePath)) === "no exist sync") {
+            storage = {
+                  algorithm_storage: []
             }
-
-            // variable representing the new json entry
-            let data = storage.algorithm_storage;
-
-            // prevent duplicate entry
-            let check = data.find(item => item.name === name)
-            if(check) {
-                  return 'Found a matching name property in the database. Please provide a unique name for your algorithm'
-            } else {
-                  let check = []
-                  let array = code.split('')
-
-                  for(let i = 0; i < array.length; i ++) {
-                        let curr = array[i]
-                        check.push(curr)
-                  }
-
-                  if(check.includes('"')) {
-                        throw new Error(`Please use ' characters in the place of " characters`)
-                  }
-            }
-
-            name = checkName(name)
-
-            if(!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(date)) {
-                  console.log("Error. Please use mm/dd/yyyy format for the date parameter")
-                  return
-            }
-
-            // if no duplicate and no ""
-            data.push({ name, date, difficulty, code })
-
-            // stringify for shipment to json file
-            const dataToJSON = JSON.stringify(storage, null, 2)
-
-            // fs method for writing to json file
-            fs.writeFile(filePath, dataToJSON, () => {
-                  console.log('Algorithm(s) saved to storage file') 
-            })
-
-            return 'Algorithm(s) saved to json storage'
+      } else if((checkPath(filePath)) === "exist sync"){
+            const contents = fs.readFileSync(filePath, 'utf8')
+            storage = JSON.parse(contents)
       } else {
-            return 'Incohesive file path'
+            return checkPath(filePath)
       }
+
+      // variable representing the new json entry
+      let data = storage.algorithm_storage;
+
+      // prevent duplicate entry
+      let check = data.find(item => item.name === name)
+      if (check) {
+            return 'Found a matching name property in the database. Please provide a unique name for your algorithm'
+      } else {
+            let check = []
+            let array = code.split('')
+
+            for (let i = 0; i < array.length; i++) {
+                  let curr = array[i]
+                  check.push(curr)
+            }
+
+            if (check.includes('"')) {
+                  throw new Error(`Please use ' characters in the place of " characters`)
+            }
+      }
+
+      name = checkName(name)
+
+      if (!/^\d{1,2}\/\d{1,2}\/\d{4}$/.test(date)) {
+            console.log("Error. Please use mm/dd/yyyy format for the date parameter")
+            return
+      }
+
+      // if no duplicate and no ""
+      data.push({ name, date, difficulty, code })
+
+      // stringify for shipment to json file
+      const dataToJSON = JSON.stringify(storage, null, 2)
+
+      // fs method for writing to json file
+      fs.writeFile(filePath, dataToJSON, () => {
+            console.log('Algorithm(s) saved to storage file')
+      })
+
+      return 'Algorithm(s) saved to json storage'
 }
 
 module.exports = storeAlgorithm;
