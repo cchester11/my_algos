261dc859528709420775641fe59b0c18aadae950
Charles <cchester1@angelo.edu>
2023-10-27 15:02:33 -0500

    algorithm stored

--- functions/store_json.js
+++ functions/store_json.js
@@ -8,10 +8,16 @@ const algoDate = process.argv[3]
 const algoDiff = process.argv[4]
 const algoCode = process.argv[5]
 
-// error checking
-if (algoName && algoDate && algoDiff && algoCode) {
-      storeAlgorithm(algoName, algoDate, algoDiff, algoCode)
-} else {
-      console.log(__dirname)
-      throw new Error('Please provide the name under which you like to the algorithm to be stored, the date, the difficulty (easy, medium, or hard), and the code itself--in that order.')
+// error checking or run the storeAlgorithm function
+try {
+      if (algoName && algoDate && algoDiff && algoCode) {
+            storeAlgorithm(algoName, algoDate, algoDiff, algoCode)
+      } else {
+            console.log(__dirname)
+            throw new Error('Please provide the name under which you like to the algorithm to be stored, the date, the difficulty (easy, medium, or hard), and the code itself--in that order.')
+      }
+} catch (err) {
+      if(err) {
+            throw new Error(err)
+      }
 }
