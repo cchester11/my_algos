4d8b50cba844230529d5492847b22a106c881b0b
Charles <cchester1@angelo.edu>
2023-10-30 17:23:06 -0500

    test jest blah

---
+++ test/test.test.js
@@ -0,0 +1,25 @@
+/**
+ * Binary search implementation.
+ * @param {*[]} array
+ * @param {*} seekEl
+ * @param {function(a, b)} [comparator]
+ * @return {number}
+ */
+
+// initiate description
+let description = '';
+
+function binarySearch (array, seekEl, comparator) {
+      // initiate the comparator function 
+
+      // declare start and end index for the search process
+
+      // write for loop used in search process
+      // return the seekEl if found
+
+      // return -1 (false) if not found
+};
+
+// give description string a value
+
+// run jest test manually
