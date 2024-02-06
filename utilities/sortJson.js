004e86726f4b4271fed76e42d40b5a8297bb7373
Charles <cchester1@angelo.edu>
2024-02-06 15:06:52 -0600

    working on sortJson utility. Use gpt from here to help finish the utility

--- utilities/sortJson.js
+++ utilities/sortJson.js
@@ -1,12 +1,28 @@
 // import json
 // import path and fs modules
-
+const path = require('path');
+const fs = require('fs');
 // declare path to data 
-
+const dataPath = path.join(__dirname, '../json/algoData.json');
 // extrapolate the algorithmStorage array from the json
+const dataExtract = fs.readFileSync(dataPath, {
+      encoding: 'utf-8'
+});
+let data = Object.entries(dataExtract.algorithmStorage).map(([key, algorithm]) => {
+      return algorithm
+})
 
+console.log(data);
 // sort the json
-
+const sortData = data.sort((a, b) => {
+      // define variables for algorithm names of a and b
+      const nameA = a.name.toLowerCase();
+      const nameB = b.name.toLowerCase();
+      // sort logic
+      if(nameA < nameB) return -1;
+      if(nameA > nameB) return 1;
+      return 0;
+})
 // rewrite the file
 
 // export this file as utility
