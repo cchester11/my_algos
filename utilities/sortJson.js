5615bb99fecf864a1f4e23c4487dfdc3d8251388
Charles <cchester1@angelo.edu>
2024-02-06 12:13:36 -0600

    comments written  for sortJson utility

---
+++ utilities/sortJson.js
@@ -0,0 +1,12 @@
+// import json
+// import path and fs modules
+
+// declare path to data 
+
+// extrapolate the algorithmStorage array from the json
+
+// sort the json
+
+// rewrite the file
+
+// export this file as utility
